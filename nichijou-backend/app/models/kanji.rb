39f03d005a809b5b78547050ec5c25ccb7462da4
Drew Thompson <drewallynthompson@gmail.com>
2021-01-21 23:59:03 -0700

    validation

--- nichijou-backend/app/models/kanji.rb
+++ nichijou-backend/app/models/kanji.rb
@@ -1,3 +1,7 @@
 class Kanji < ApplicationRecord
     belongs_to :user
+    validates :name, presence: true,
+        length: { minimum: 2, maximum: 254},
+        format: { with: /\A[一-龯ぁ-ゔゞァ-・ヽヾ゛゜ーa-zA-Z]/ },
+        uniqueness: true
 end
--- nichijou-backend/app/models/user.rb
+++ nichijou-backend/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_many :notes, :kanji
+    has_secure_password
+    validates :username, presence: true,
+        length: { minimum: 2, maximum: 254},
+        format: { with: /\A[一-龯ぁ-ゔゞァ-・ヽヾ゛゜ーa-zA-Z]/ },
+        uniqueness: true
+    validates :password, presence: true
 end
