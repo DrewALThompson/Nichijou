4ae3cecb9b62a2087406e3d72d5865a8f231e7ce
Drew Thompson <drewallynthompson@gmail.com>
2021-01-22 01:02:54 -0700

    Calender Idea

--- nichijou-backend/app/controllers/application_controller.rb
+++ nichijou-backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    protect_from_forgery with: exception
+    helper_method :current_user, :require_login
+
+    def current_user
+
+    end
+
 end
