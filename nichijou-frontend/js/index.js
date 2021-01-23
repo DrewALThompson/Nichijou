2d0c0cdf1c3f9df96a8a7abf7bd1d4eabea4bccf
Drew Thompson <drewallynthompson@gmail.com>
2021-01-23 01:41:47 -0700

    Cal pt 2:functions

--- nichijou-frontend/js/index.js
+++ nichijou-frontend/js/index.js
@@ -1,10 +1,54 @@
+
+// CALENDAR FUNCTIONS
+
 let tblbody = document.getElementById('calbody'),
-  currentDate = new Date(),
-  month = currentDate.getMonth(),
-  year = currentDate.getFullYear();
+  crntDate = new Date(),
+  month = crntDate.getMonth(),
+  year = crntDate.getFullYear();
   sltYr = document.getElementById('year'),
-  sltMnth = document.getElementById('month');
+  sltMnth = document.getElementById('month'),
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+function next() => {
+  if (month === 11){
+      year++;
+      month = 0;
+  } else {
+      month++;
+  }
+  calendar(month, year);
+}
+
+function previous() => {
+    if (month === 0){
+        year--;
+        month = 11;
+    } else {
+        month--;
+    }
+    calendar(month, year);
+}
+
+function year() => {
+    year = parseInt(sltYr.value);
+    calendar(month, year);
+}
+
+function month() => {
+    month = parseInt(sltMnth.value);
+    calendar(month, year);
+}
+
+function calendar(month, year){
+
+}
+
+// CALENDAR FUNCTIONS
+
+// DOMLOAD FUNCTION
 
 document.addEventListener('DOMContentLoaded', () =>{
   
 })
+
+// DOMLOAD FUNCTION
