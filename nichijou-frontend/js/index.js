9431456de845b90b69769431822578d18a305bf0
Drew Thompson <drewallynthompson@gmail.com>
2021-01-23 01:19:48 -0700

    Cal pt 1

---
+++ nichijou-frontend/js/index.js
@@ -0,0 +1,10 @@
+let tblbody = document.getElementById('calbody'),
+  currentDate = new Date(),
+  month = currentDate.getMonth(),
+  year = currentDate.getFullYear();
+  sltYr = document.getElementById('year'),
+  sltMnth = document.getElementById('month');
+
+document.addEventListener('DOMContentLoaded', () =>{
+  
+})
